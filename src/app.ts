e8b44cc87a6265365a363f3eb676328765caea68
Angelo Gonçalves da Luz <angelogl@gmail.com>
2020-05-26 14:38:48 -0300

    :wrench: Update configuration files

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express from 'express';
-// import routes from './routes';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
-// app.use(routes);
+app.use(routes);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import app from './app';
+import 'reflect-metadata';
+import './database';
 
 app.listen(3000, () => {
   console.log('🏃 Running Server');
