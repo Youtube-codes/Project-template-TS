ace3fbc92cba642b60275d6beb9b53dbca607215
Angelo Gonçalves da Luz <angelogl@gmail.com>
2020-05-26 18:28:42 -0300

    :sparkles: Add migrations to project

---
+++ src/migrations/1590517456673-createClass.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateClass1590517456673 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'class',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'duration',
+            type: 'integer',
+          },
+          {
+            name: 'created_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'update_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('class');
+  }
+}
